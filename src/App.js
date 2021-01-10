5622a5b1279a817cc06fc8f94df712f9e4ff2799
allison <allisonmspeck@gmail.com>
2021-01-10 16:36:24 -0500

    [#jf26t8] Add Login Form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-dom';
+import Login from './views/auth/Login';
+import Dashboard from './views/Dashboard/Index';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <div>
+                <NavLink to='/dashboard'>Dashboard</NavLink>
+                <NavLink to='/login'>Login</NavLink>
+            </div>
+            <Switch>
+                <Route path='/dashboard' component={Dashboard} />
+                <Route path='/login' component={Login} />
+            </Switch>
+        </Router>
+    );
+};
 
 export default App;
