80b184a40de840089c443f0f27a84f806c314a62
allison <allisonmspeck@gmail.com>
2021-01-17 14:08:30 -0500

    [#jyw596] Setup Redux
    installed thunk, redux + refactored Login component to handle state using Reducers.

---
+++ src/store/Store.js
@@ -0,0 +1,11 @@
+import thunk from 'redux-thunk';
+import {applyMiddleware, createStore} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {rootReducer} from "../reducers/RootReducer";
+
+const configStore = ()=>{
+    const composeEnhancers = composeWithDevTools(applyMiddleware(thunk));
+    return createStore(rootReducer, {}, composeEnhancers);
+};
+
+export const store = configStore();
