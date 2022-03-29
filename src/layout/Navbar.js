3f49f164981440bed90cd57191928b94e8b0ab11
allison <allisonmspeck@gmail.com>
2022-03-29 19:39:04 -0400

    #25eacrj show profile
    hide login/register on successful login

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {NavLink} from "react-router-dom";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faBars, faDog} from "@fortawesome/free-solid-svg-icons";
+import {faBars, faDog, faUserCircle} from "@fortawesome/free-solid-svg-icons";
+import {connect} from "react-redux";
 
 
-const Navbar = () => {
+const Navbar = ({isLoggedIn,user}) => {
     const [show, setShow] = React.useState(false);
+
 
     return (
 
@@ -15,28 +17,49 @@ const Navbar = () => {
                 PupDates
             </div>
 
+
+            {
+                isLoggedIn &&
+                    <NavLink className="flex justify-end self-center mx-1 font-bold hover:text-gray-700" to=''>{user.name}
+                        <FontAwesomeIcon className="ml-1 text-2xl cursor-pointer" icon={faUserCircle}/>
+                    </NavLink>
+            }
+            {
+                !isLoggedIn && <>
+
             {/*if mobile*/}
-            <div className="flex justify-end hidden md:block">
+                <div className="hidden md:flex md:justify-end">
                 <NavLink className="self-center mx-1 font-bold" to='/login'>Login</NavLink>
                 <NavLink className="self-center mx-1" to='/register'>Register</NavLink>
-            </div>
+                </div>
 
-            <div className="flex justify-end md:hidden">
+                <div className="flex justify-end md:hidden">
                 <button className="px-2 text-gray-800 focus:outline-gray" onClick={() => setShow(!show)}>
-                    <FontAwesomeIcon icon={faBars}/>
+                <FontAwesomeIcon icon={faBars}/>
                 </button>
-            </div>
+                </div>
             {
                 show &&
 
                 <div className="w-full flex flex-col justify-center pt-2 md:hidden">
-                    <NavLink className="self-center" to='/login'>Login</NavLink>
-                    <NavLink className="self-center" to='/register'>Register</NavLink>
+                <NavLink className="self-center" to='/login'>Login</NavLink>
+                <NavLink className="self-center" to='/register'>Register</NavLink>
                 </div>
             }
+                </>
+            }
+
         </div>
     )
 };
 
+const mapStateToProps = (state) => {
+    return {
+        error: state.userState.error,
+        isLoggedIn: state.userState.isLoggedIn,
+       user:state.userState.user
+    }
+};
 
-export default Navbar;
+export default connect(mapStateToProps)(Navbar);
+// export default Navbar;
